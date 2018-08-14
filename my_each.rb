bce320f2cda62fb266e6a2bfa8acc269fe7ba202
Mendy <mendybeltcohen@gmail.com>
2018-08-14 18:17:23 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,13 @@
 def my_each(array) # put argument(s) here
   # code here
   if block_given?
-    
+    i = 0
+    while i <array.length
+    yield array[i]
+    i+=1
+  end
+  array
+ end
 end
 
 def hello_t(array)
