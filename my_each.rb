3f7b6f2e372b5555ebc456443e4e53d1f2938851
Mendy <mendybeltcohen@gmail.com>
2018-08-14 18:17:29 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -10,15 +10,15 @@ def my_each(array) # put argument(s) here
  end
 end
 
-def hello_t(array)
-  if block_given?
-  i = 0
- while i < array.length
-  yield array[i]
-   i+=1
- end 
- array
- else
-   puts "Hey! No block was given!"
- end
+# def hello_t(array)
+#   if block_given?
+#   i = 0
+# while i < array.length
+#   yield array[i]
+#   i+=1
+# end 
+# array
+# else
+#   puts "Hey! No block was given!"
+# end
 end
